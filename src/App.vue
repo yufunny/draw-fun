de333603efc0675968de5b9301fcc2c641c62eaf
XyinM <hdumxy@163.com>
2016-10-18 12:52:48 +0800

    修复宽屏适配的问题

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="view">
     <home v-if="showPage == 'home'"></home>
     <wait v-if="showPage == 'wait'"></wait>
     <draw v-if="showPage == 'draw'"></draw>
